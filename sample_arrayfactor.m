3c71acea1894c2f7e41646535735b55e71273c18
Kanta Terui <114731966+K-Terui@users.noreply.github.com>
2023-07-18 02:36:06 +0900

    Add polar plot

--- sample_arrayfactor.m
+++ sample_arrayfactor.m
@@ -10,14 +10,18 @@ k0=2*pi; %wavenumber
 phi_deg=0.;                   %[deg]
 phi=phi_deg*(pi/180.);        %[rad]
 %azimuth
-theta = (0 : 180)/180*pi;     %[rad]
+theta = (0 : 360)/180*pi;     %[rad]
 arr_theta_deg = theta/pi*180; %[deg]
-n=6;
+n=16;
 d=0.5;
 
 %% complex weight
 wuni = ones(1,6);   %uniform distribution
-wbin = [1 4 6 4 1]; %binomial distribution
+%binomial distribution
+wbin = sym([]);
+for k = 0:n-1
+    wbin = horzcat(wbin, nchoosek(n-1, k));
+end
 
 %% array factor
 AFuni = arrayfactor(wuni, theta, phi);
@@ -47,3 +51,22 @@ ax.XAxis.FontSize = 15;
 ax.YAxis.FontSize = 15;
 xlabel('Azimuth [deg]', 'Interpreter', 'latex')
 ylabel('Array gain [dB]', 'Interpreter', 'latex')
+
+figure(2) %with normalization
+rmin = -40; %minimum value of r axis
+rmax = 0;   %maximum value of r axis
+%converted to dB value and normalization
+AFunidB = 20*log10(AFuni/max(AFuni));
+AFbindB = 20*log10(AFbin/max(AFbin));
+%calcualte the minimum value to satisfy the r-axis threshold
+unimin = min(AFunidB(find(AFunidB>rmin)));
+unibin = min(AFbindB(find(AFbindB>rmin)));
+%rewrite values below the r-axis threshold to the smallest value above the r-axis threshold
+AFunidB(find(AFunidB<rmin)) = unimin;    
+AFbindB(find(AFbindB<rmin)) = unibin;
+
+ppuni = polarplot(theta, AFunidB, '-' , 'Color', [0 0.5 0], 'LineWidth', 2);
+hold on
+ppbin = polarplot(theta, AFbindB, '-.', 'Color', [1 0.5 0], 'LineWidth', 2);
+legend([ppbin, ppuni], {'Binomial beam', 'Uniform dist.'}, 'Interpreter', 'latex', 'FontSize', 15, 'Position',[0.63,0.77,0.25,0.10])
+rlim([rmin rmax])
