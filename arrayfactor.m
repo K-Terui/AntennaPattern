dc1f9f7268037d23b8c0a2b03d28902aeef5fb15
Kanta Terui <114731966+K-Terui@users.noreply.github.com>
2023-07-20 14:47:11 +0900

    add dBi evaluation

--- arrayfactor.m
+++ arrayfactor.m
@@ -1,4 +1,4 @@
-function absAF = arrayfactor(w, theta, phi)
+function AF = arrayfactor(w, theta, phi)
 % calculate the array factor for directivity plot
 % considering: ULA
 % Input
@@ -13,7 +13,7 @@ d  = 0.5;
 w  = reshape(w, 1, []);
 n  = size(w, 2);
 theta = reshape(theta, 1, []);
-
+AFiso = ones(length(theta),1); %isotropic antenna gain
 %% positions of each element
 for i=1:n
     x(i)=0.; y(i)=0.; z(i)=(i-1)*d; 
@@ -22,4 +22,7 @@ end
 %% array factor
 absAF = abs(sum(w .* exp(1i * k0 * (z .* cos(theta'))), 2)); %ULA
 % absAF = abs(sum(w .* exp(1i * k0 * (sin(theta') .* (x * cos(phi) + y * sin(phi)) + z .* cos(theta'))), 2)); %UPA
+
+%% normalized array factor with isotropic antenna
+AF = absAF ./ AFiso;
 end
